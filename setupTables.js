77982caccea909c8283aea5ff1ed04a704969b05
Ivan Gonzalez <navi@pop-os.localdomain>
2021-09-20 17:38:00 -0600

    * KMS-9 moved setup tables script from TS to JS

--- setupTables.js
+++ setupTables.js
@@ -1,110 +1,57 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-exports.__esModule = true;
-var client_dynamodb_1 = require("@aws-sdk/client-dynamodb");
-var ddbClient = new client_dynamodb_1.DynamoDBClient({ region: "us-west-2", endpoint: "http://127.0.0.1:8000" });
-var params = {
+import { DynamoDBClient, PutItemCommand, CreateTableCommand, ListTablesCommand } from "@aws-sdk/client-dynamodb";
+
+const ddbClient = new DynamoDBClient({region: "us-west-2", endpoint: "http://127.0.0.1:8000"});
+
+const params = {
     AttributeDefinitions: [
-        {
-            AttributeName: "Season",
-            AttributeType: "N"
-        },
-        {
-            AttributeName: "Episode",
-            AttributeType: "N"
-        },
+      {
+        AttributeName: "Season", //ATTRIBUTE_NAME_1
+        AttributeType: "N", //ATTRIBUTE_TYPE
+      },
+      {
+        AttributeName: "Episode", //ATTRIBUTE_NAME_2
+        AttributeType: "N", //ATTRIBUTE_TYPE
+      },
     ],
     KeySchema: [
-        {
-            AttributeName: "Season",
-            KeyType: "HASH"
-        },
-        {
-            AttributeName: "Episode",
-            KeyType: "RANGE"
-        },
+      {
+        AttributeName: "Season", //ATTRIBUTE_NAME_1
+        KeyType: "HASH",
+      },
+      {
+        AttributeName: "Episode", //ATTRIBUTE_NAME_2
+        KeyType: "RANGE",
+      },
     ],
     ProvisionedThroughput: {
-        ReadCapacityUnits: 1,
-        WriteCapacityUnits: 1
+      ReadCapacityUnits: 1,
+      WriteCapacityUnits: 1,
     },
-    TableName: "TEST_TABLE",
+    TableName: "TEST_TABLE", //TABLE_NAME
     StreamSpecification: {
-        StreamEnabled: false
+      StreamEnabled: false,
+    },
+  };
+
+const createTable = async () => {
+    try {
+        const data = await ddbClient.send(new CreateTableCommand(params));
+        console.log("Table Created", data);
+        return data;
+    } catch (err) {
+        console.log("Error: ", err);
     }
-};
-var createTable = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var data, err_1;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, ddbClient.send(new client_dynamodb_1.CreateTableCommand(params))];
-            case 1:
-                data = _a.sent();
-                console.log("Table Created", data);
-                return [2 /*return*/, data];
-            case 2:
-                err_1 = _a.sent();
-                console.log("Error: ", err_1);
-                return [3 /*break*/, 3];
-            case 3: return [2 /*return*/];
-        }
-    });
-}); };
-var listTables = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var data, err_2;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, ddbClient.send(new client_dynamodb_1.ListTablesCommand({}))];
-            case 1:
-                data = _a.sent();
-                console.log(data.TableNames.join("\n"));
-                return [2 /*return*/, data];
-            case 2:
-                err_2 = _a.sent();
-                console.error(err_2);
-                return [3 /*break*/, 3];
-            case 3: return [2 /*return*/];
-        }
-    });
-}); };
-createTable();
-listTables();
+}
+
+const listTables = async () => {
+    try {
+      const data = await ddbClient.send(new ListTablesCommand({}));
+      console.log(data.TableNames.join("\n"));
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+await createTable();
+await listTables();
