474b4f5fa785ac74f6314948e3eda18b81f84fc4
Ivan Gonzalez <navi@pop-os.localdomain>
2021-09-21 22:19:57 -0600

    * KMS-3 Added DynamoDB table and sample Lambda

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,41 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { RemovalPolicy } from "@aws-cdk/core";
+import {
+  AttributeType,
+  BillingMode,
+  StreamViewType,
+  Table,
+  TableEncryption,
+} from "@aws-cdk/aws-dynamodb";
+import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
+import { Runtime } from "@aws-cdk/aws-lambda";
+import { join } from "path";
 
-export class CdkStack extends cdk.Stack {
+export class KafkaStoreAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /**
+     * Database
+     */
+    const orderTable = new Table(this, "OrderTable", {
+      partitionKey: { name: "pk", type: AttributeType.NUMBER },
+      sortKey: { name: "sk", type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      pointInTimeRecovery: true,
+      encryption: TableEncryption.AWS_MANAGED,
+      stream: StreamViewType.NEW_AND_OLD_IMAGES,
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
+
+    const sampleFunction = new NodejsFunction(this, "SampleFunction", {
+      runtime: Runtime.NODEJS_14_X,
+      handler: "handler",
+      entry: join(__dirname, "../../src/sample-lambda.ts"),
+      bundling: {
+        target: "node14",
+        externalModules: ["aws-sdk"],
+      },
+    });
   }
 }
